c6ffca148e6ebdcb4cd2f08311e69c347f96961b
skylineone044 <tokodioli@gmail.com>
2019-03-30 11:21:06 +0100

    Added linking for konsole

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo Started linker script...
+
+echo linking vim...
+ln -s ~/git/dotfiles/vim/.vim ~/.vim
+ln -s ~/git/dotfiles/vim/.vimrc ~/.vimrc
+
+echo linking tmux...
+ln -s ~/git/dotfiles/tmux/.tmux ~/.tmux
+ln -s ~/git/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+echo linking zsh...
+ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
+
+echo linking latte layout...
+ln -s ~/git/dotfiles/latte ~/.config/latte
+
+echo linking konsole...
+ln -s ~/git/dotfiles/konsole/ZSH_FTW.profile ~/.local/share/konsole/ZSH_FTW.profile
+ln -s ~/git/dotfiles/konsole/skykonsole.colorscheme ~/.local/share/konsole/skykonsole.colorscheme
+
+
+echo Getting dependencies...
+echo getting POWERLEVEL9K...
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo getting essentials: tmux, zsh, vim...
+sudo apt install tmux zsh vim || sudo pacman -S tmux zsh vim
+echo getting vim python helpers...
+echo  getting pylint...
+sudo apt install python-pylint python3-pylint || sudo apt install python2-pylint python-pylint || sudo pacman -S python-pylint python3-pylint || sudo pacman -S python2-pylint python-pylint
+echo getting python-jedi
+sudo apt install python-jedi python3-jedi || sudo apt install python2-jedi python-jedi || sudo pacman -S python-jedi python3-jedi || sudo pacman -S python2-jedi python-jedi
+
+echo Done!
+
