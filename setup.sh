4c9db671924f542495f84ca487288d193c3401ee
skylineone044 <skylineone044@gmail.com>
2022-10-08 15:34:35 +0200

    enabled system configs

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ CLEAR_COLOR='\033[0m' # No Color
 
 alias print_green="printf \"$BOLD_GREEN\"%s\"$CLEAR_COLOR\"\\\\n"
 
+./apply_system_configs.sh
 ./install_apps.sh
 ./link_configs.sh
 ./set_desktop_theme.sh
