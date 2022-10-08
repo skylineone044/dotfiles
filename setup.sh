f6c4e69678fd55fc44553688fd3c341f7779a922
skylineone044 <skylineone044@gmail.com>
2022-10-08 15:52:37 +0200

    export variables so they are available in the subshells

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-BOLD_GREEN='\033[1;32m'
-CLEAR_COLOR='\033[0m' # No Color
+export BOLD_GREEN='\033[1;32m'
+export CLEAR_COLOR='\033[0m' # No Color
 
-PRINT_COLORED="printf \"$BOLD_GREEN\"%s\"$CLEAR_COLOR\"\\\\n"
+export PRINT_COLORED="printf $BOLD_GREEN%s$CLEAR_COLOR\\n"
 
 ./apply_system_configs.sh
 ./install_apps.sh
 ./link_configs.sh
 ./set_desktop_theme.sh
 
-$PRINT_COLORED "All done$CLEAR_COLOR"
+$PRINT_COLORED "All done"
+
